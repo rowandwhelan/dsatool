9ffec97425b6d0d5b3bb6361fae6663f109e4266
Rowan Whelan <96452323+DedbotGit@users.noreply.github.com>
2024-01-10 10:08:48 -0600

    Added new files

--- econ.js
+++ econ.js
@@ -2,27 +2,41 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(customParseFormat);
 
 econInit();
-function econInit(startDay, startMonth, startYear, endDay, endMonth, endYear) {
-  let timeSlot = 3;
+
+async function econInit(
+  startDay,
+  startMonth,
+  startYear,
+  endDay,
+  endMonth,
+  endYear,
+) {
+  let buffer = 1;
+  let timeSlot = 7;
+  if (timeSlot <= 1) {
+    timeSlot = 1;
+  }
   if (
     startDay === undefined ||
     startMonth === undefined ||
     startYear === undefined
   ) {
-    startDay = dayjs().subtract(1, "days").format("DD");
-    startMonth = dayjs().subtract(1, "days").format("MM");
-    startYear = dayjs().subtract(1, "days").format("YYYY");
+    const yesterday = dayjs().subtract(buffer, "days");
+    startDay = yesterday.format("D");
+    startMonth = yesterday.format("M");
+    startYear = yesterday.format("YYYY");
   }
 
   if (endDay === undefined || endMonth === undefined || endYear === undefined) {
-    endDay = dayjs().subtract(1+timeSlot, "days").format("DD");
-    endMonth = dayjs().subtract(1+timeSlot, "days").format("MM");
-    endYear = dayjs().subtract(1+timeSlot, "days").format("YYYY");
+    const endSlot = dayjs().subtract(buffer + timeSlot, "days");
+    endDay = endSlot.format("D");
+    endMonth = endSlot.format("M");
+    endYear = endSlot.format("YYYY");
   }
-  econGetDate(startDay, startMonth, startYear, endDay, endMonth, endYear);
+  await econGetDate(startDay, startMonth, startYear, endDay, endMonth, endYear);
 }
 
-function econGetDate(
+async function econGetDate(
   startDay,
   startMonth,
   startYear,
@@ -33,55 +47,57 @@ function econGetDate(
   let endDate = dayjs(`${endYear}-${endMonth}-${endDay}`);
   let currentDate = dayjs(`${startYear}-${startMonth}-${startDay}`);
 
+  let datesToFetch = [];
   while (currentDate.isAfter(endDate) || currentDate.isSame(endDate)) {
-    let formattedDate = currentDate.format("YYYY_MM_DD");
-    econGet(formattedDate);
+    let formattedDate = currentDate.format("YYYY_M_D");
+    datesToFetch.push(formattedDate);
     currentDate = currentDate.subtract(1, "day");
+  }
+
+  for (const date of datesToFetch) {
+    await econGet(date);
   }
 }
 
-function econGet(date) {
-  console.log(date);
-  const apiUrl =
-    "https://uncors.vercel.app/?url=https://pub.drednot.io/prod/econ/" +
-    date +
-    "/summary.json";
+async function econGet(date) {
+  const apiUrl = `https://uncors.vercel.app/?url=https://pub.drednot.io/prod/econ/${date}/summary.json`;
 
-  let econ = {};
-  let items = {};
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      econ = data;
-      fetch(
-        "https://uncors.vercel.app/?url=https://pub.drednot.io/prod/econ/item_schema.json"
-      )
-        .then((response) => response.json())
-        .then((itemData) => {
-          items = itemData;
-          output(econ, items);
-        })
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      let econ = data;
+      const itemDataResponse = await fetch(
+        "https://uncors.vercel.app/?url=https://pub.drednot.io/prod/econ/item_schema.json",
+      );
+      if (itemDataResponse.ok) {
+        const itemData = await itemDataResponse.json();
+        output(econ, itemData, date);
+      } else {
+        console.error("Failed to fetch item schema data");
+      }
+    } else {
+      console.error("Failed to fetch econ data");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
-function output(econ, items) {
-  console.log(econ);
-  console.log(items);
+function output(econ, items, date) {
+  let outputDate = dayjs(date, "YYYY_M_D").format("ddd[,] DD MMM YYYY")
+  document.getElementById("output").innerHTML += `<br><h2>${outputDate}<h2><br>`;
   for (const [key, value] of Object.entries(econ.items_held)) {
     let itemNum = value;
-    let itemName;
-    for (let i = 0; i < items.length; i++) {
+    let i = 0;
+    while (i < items.length) {
       if (items[i].id == key) {
-        itemName = items[i].name;
-        i = items.length + 1;
+        document.getElementById("output").innerHTML +=
+          `${itemNum} ${items[i].name}<br>`;
+        console.log(`${itemNum} ${items[i].name}`);
+        break;
       }
-      if (!(itemNum == undefined || itemName == undefined)) {
-        console.log(itemNum + " " + itemName);
-      }
+      i++;
     }
   }
 }
