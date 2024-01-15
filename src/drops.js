f9629df118473c6bb9d58ecdc914cafab3c72de0
Rowan Whelan <96452323+DedbotGit@users.noreply.github.com>
2024-01-15 14:51:35 -0600

    Update drops.js

--- src/drops.js
+++ src/drops.js
@@ -1,7 +1,7 @@
 async function dropsGet() {
   try {
     const response = await fetch(`https://uncors.vercel.app/?url=https://pub.drednot.io/prod/econ/bot_drops.txt`);
-      const data = await response.json();
+      const data = await response;
          document.getElementById("output").innerHTML +=
           `${data}<br>`;
         console.log(`${data}`);
