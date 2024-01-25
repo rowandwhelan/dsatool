47aab25d1dadf1747d3d2aaf7ae4ba15808261e0
Rowan Whelan <96452323+DedbotGit@users.noreply.github.com>
2024-01-25 13:24:21 -0600

    Update econBundle.js

--- public/econBundle.js
+++ public/econBundle.js
@@ -7,39 +7,66 @@ const dayjs = require('dayjs');
 const customParseFormat = require("dayjs/plugin/customParseFormat");
 dayjs.extend(customParseFormat);
 
-let timeSlot;
+let timeSlot = 0;
+document.getElementById("start").addEventListener("change", econInit);
+document.getElementById("end").addEventListener("change", econInit);
 econInit();
 
-async function econInit(
-  startDay,
-  startMonth,
-  startYear,
-  endDay,
-  endMonth,
-  endYear
-) {
-  let buffer = 1;
-  timeSlot = 14;
-  if (timeSlot <= 1) {
-    timeSlot = 1;
-  }
-  if (
-    startDay === undefined ||
-    startMonth === undefined ||
-    startYear === undefined
-  ) {
-    const yesterday = dayjs().subtract(buffer, "days");
+async function econInit() {
+  econDataFetched = false;
+  days = [];
+  iron = [];
+  exp = [];
+  flux = [];
+  rubber = [];
+  cores = [];
+  gens = [];
+  projs = [];
+  rcs = [];
+  auto = [];
+  burst = [];
+  goldNull = [];
+  silverNull = [];
+  bug = [];
+  shredder = [];
+  legacy = [];
+  glass = [];
+  rcd = [];
+  backpack = [];
+  bp = [];
+  manifest = [];
+  bom = [];
+  backpack = [];
+  volleyball = [];
+  basketball = [];
+  beachball = [];
+  football = [];
+  rapid = [];
+  pres = [];
+  controller = [];
+  
+  let startDate = dayjs(document.getElementById("start").value, "YYYY-MM-DD");
+  let endDate = dayjs(document.getElementById("end").value, "YYYY-MM-DD");
+  let startDay = startDate.date();
+  let endDay = endDate.date();
+  let startMonth = startDate.month();
+  let endMonth = endDate.month();
+  let startYear = startDate.year();
+  let endYear = endDate.year();
+  econDataFetched = false;
+  if (document.getElementById("start").value === '') {
+    const yesterday = dayjs().subtract(1, "days");
     startDay = yesterday.format("D");
     startMonth = yesterday.format("M");
     startYear = yesterday.format("YYYY");
   }
-
-  if (endDay === undefined || endMonth === undefined || endYear === undefined) {
-    const endSlot = dayjs().subtract(buffer + timeSlot, "days");
+  if (document.getElementById("end").value === '') {
+    const endSlot = dayjs().subtract(15, "days");
     endDay = endSlot.format("D");
     endMonth = endSlot.format("M");
     endYear = endSlot.format("YYYY");
   }
+
   await econGetDate(startDay, startMonth, startYear, endDay, endMonth, endYear);
 }
 
@@ -58,8 +85,9 @@ async function econGetDate(
   let datesToFetch = [];
   while (currentDate.isAfter(endDate) || currentDate.isSame(endDate)) {
     let formattedDate = currentDate.format("YYYY_M_D");
-    datesToFetch.push(formattedDate);
+    datesToFetch.unshift(formattedDate);
     currentDate = currentDate.subtract(1, "day");
+    timeSlot++;
   }
 
   for (const date of datesToFetch) {
@@ -76,7 +104,6 @@ async function econGetDate(
     await econGet(date);
   }
   econDataFetched = true;
-  econInit = true;
   graphChart();
   document.getElementById("out").innerHTML = '';
 }
@@ -106,48 +133,46 @@ async function econGet(date) {
   }
 }
   
-var econDataFetched = false;
-var econInit = false;
-var days = [];
-var iron = [];
-var exp = [];
-var flux = [];
-var rubber = [];
-var cores = [];
-var gens = [];
-var projs = [];
-var rcs = [];
-var auto = [];
-var burst = [];
-var goldNull = [];
-var silverNull = [];
-var bug = [];
-var shredder = [];
-var legacy = [];
-var glass = [];
-var rcd = [];
-var backpack = [];
-var bp = [];
-var manifest = [];
-var bom = [];
-var backpack = [];
-var volleyball = [];
-var basketball = [];
-var beachball = [];
-var football = [];
-var rapid = [];
-var pres = [];
-var controller = [];
+var econDataFetched
+var econInit
+var days 
+var iron 
+var exp 
+var flux 
+var rubber 
+var cores 
+var gens 
+var projs 
+var rcs 
+var auto 
+var burst 
+var goldNull 
+var silverNull 
+var bug 
+var shredder 
+var legacy 
+var glass 
+var rcd 
+var backpack 
+var bp 
+var manifest 
+var bom 
+var backpack 
+var volleyball 
+var basketball 
+var beachball 
+var football 
+var rapid 
+var pres 
+var controller 
 function output(econ, items, date) {
   let outputDate = dayjs(date, "YYYY_M_D").format("ddd[,] DD MMM YYYY")
-    //document.getElementById("output").innerHTML += `<br><h2>${outputDate}<h2><br>`;
     days.push(outputDate)
     for (const [key, value] of Object.entries(econ.items_held)) {
       let itemNum = value;
       let i = 0;
       while (i < items.length) {
         if (items[i].id == key) {
-          //document.getElementById("output").innerHTML += `${itemNum} ${items[i].name} ${key}<br>`;
           if (items[i].name == 'Iron') {
             iron.push(itemNum)
           } else if (items[i].name == 'Explosives') {
@@ -216,86 +241,49 @@ document.addEventListener("change", graphChart);
   
     function graphChart() {
       if (econDataFetched) {
-        if (econInit) {
-          days.reverse();
-          iron.reverse();
-          exp.reverse();
-          flux.reverse();
-          rubber.reverse();
-          cores.reverse();
-          gens.reverse();
-          projs.reverse();
-          rcs.reverse();
-          auto.reverse();
-          burst.reverse();
-          goldNull.reverse();
-          silverNull.reverse();
-          bug.reverse();
-          shredder.reverse();
-          legacy.reverse();
-          glass.reverse();
-          rcd.reverse();
-          backpack.reverse();
-          bp.reverse();
-          manifest.reverse();
-          bom.reverse();
-          backpack.reverse();
-          volleyball.reverse();
-          basketball.reverse();
-          beachball.reverse();
-          football.reverse();
-          rapid.reverse();
-          pres.reverse();
-          controller.reverse();
-          econInit = false;
-        }
       var graphDataSet = [];
+      var borderWidth = 3;
+      var pointRadius = 3;
       if (document.getElementById("mats").checked) {
-        graphDataSet.push({ label: 'Iron', data: iron, borderWidth: 4 }, { label: 'Explosives', data: exp, borderWidth: 4 }, { label: 'Hyper Rubber', data: rubber , borderWidth: 4 }, { label: 'Flux Crystals', data: flux , borderWidth: 4 })
+        graphDataSet.push({ label: 'Iron', data: iron, borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Explosives', data: exp, borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Hyper Rubber', data: rubber , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Flux Crystals', data: flux , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("shields").checked) {
-        graphDataSet.push({ label: 'Shield Cores', data: cores , borderWidth: 4 }, { label: 'Shield Generators', data: gens , borderWidth: 4 }, { label: 'Shield Projectors', data: projs , borderWidth: 4 })
+        graphDataSet.push({ label: 'Shield Cores', data: cores , borderWidth: borderWidth, pointStyle: false }, { label: 'Shield Generators', data: gens , borderWidth: borderWidth, pointStyle: false }, { label: 'Shield Projectors', data: projs , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("turrets").checked) {
-        graphDataSet.push({ label: 'RC Turret', data: rcs , borderWidth: 4 }, { label: 'Auto Turret', auto: auto , borderWidth: 4 }, { label: 'Burst Turret', burst: burst , borderWidth: 4 })
+        graphDataSet.push({ label: 'RC Turret', data: rcs , borderWidth: borderWidth, pointStyle: false }, { label: 'Auto Turret', auto: auto , borderWidth: borderWidth, pointStyle: false }, { label: 'Burst Turret', burst: burst , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("rares").checked) {
-        graphDataSet.push({ label: 'Gold Null Trophy', data: goldNull , borderWidth: 4 }, { label: 'Silver Null Trophy', data: silverNull , borderWidth: 4 }, { label: 'Bug Hunter Trophy', data: bug , borderWidth: 4 }, { label: 'Golden Shredder', data: shredder , borderWidth: 4 }, { label: 'Legacy Fabricator', data: legacy , borderWidth: 4 })
+        graphDataSet.push({ label: 'Gold Null Trophy', data: goldNull , borderWidth: borderWidth, pointStyle: false }, { label: 'Silver Null Trophy', data: silverNull , borderWidth: borderWidth, pointStyle: false }, { label: 'Bug Hunter Trophy', data: bug , borderWidth: borderWidth, pointStyle: false }, { label: 'Golden Shredder', data: shredder , borderWidth: borderWidth, pointStyle: false }, { label: 'Legacy Fabricator', data: legacy , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("misc").checked) {
-        graphDataSet.push({ label: 'Ice Glass', data: glass , borderWidth: 4 }, { label: 'Flux RCD', data: rcd , borderWidth: 4 }, { label: 'Backpack', data: backpack , borderWidth: 4 })
+        graphDataSet.push({ label: 'Ice Glass', data: glass , borderWidth: borderWidth, pointStyle: false }, { label: 'Flux RCD', data: rcd , borderWidth: borderWidth, pointStyle: false }, { label: 'Backpack', data: backpack , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("scanners").checked) {
-        graphDataSet.push({ label: 'Blueprint Scanner', data: bp , borderWidth: 4 }, { label: 'Manifest Scanner', data: manifest , borderWidth: 4 }, { label: 'BoM Scanner', data: bom , borderWidth: 4 })
+        graphDataSet.push({ label: 'Blueprint Scanner', data: bp , borderWidth: borderWidth, pointStyle: false }, { label: 'Manifest Scanner', data: manifest , borderWidth: borderWidth, pointStyle: false }, { label: 'BoM Scanner', data: bom , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("balls").checked) {
-        graphDataSet.push({ label: 'Volleyball', data: volleyball , borderWidth: 4 }, { label: 'Basketball', data: basketball , borderWidth: 4 }, { label: 'Beach Ball', data: beachball , borderWidth: 4 }, { label: 'Football', data: football , borderWidth: 4 })
+        graphDataSet.push({ label: 'Volleyball', data: volleyball , borderWidth: borderWidth, pointStyle: false }, { label: 'Basketball', data: basketball , borderWidth: borderWidth, pointStyle: false }, { label: 'Beach Ball', data: beachball , borderWidth: borderWidth, pointStyle: false }, { label: 'Football', data: football , borderWidth: borderWidth, pointStyle: false })
       }
       if (document.getElementById("vulture").checked) {
-        graphDataSet.push({ label: 'Turret Booster - Rapid Fire', data: rapid , borderWidth: 4 }, { label: 'Turret Booster - Preservation', data: pres , borderWidth: 4 }, { label: 'Enhanced Turret Controller', data: controller , borderWidth: 4 })
+        graphDataSet.push({ label: 'Turret Booster - Rapid Fire', data: rapid , borderWidth: borderWidth, pointStyle: false }, { label: 'Turret Booster - Preservation', data: pres , borderWidth: borderWidth, pointStyle: false }, { label: 'Enhanced Turret Controller', data: controller , borderWidth: borderWidth, pointStyle: false })
       }
-      const graphDays = days ;
       if (Chart.getChart("chart") !=  undefined){
       Chart.getChart("chart").destroy();
       }
      new Chart(document.getElementById('chart'), {
         type: 'line',
         data: {
-          labels: graphDays,
+          labels: days,
           datasets: graphDataSet,
           options: {
-            responsive: true,
             interaction: {
-              mode: 'index'
-            },
+              mode: 'x',
+              intersection: false
+            }, 
             scales: {
               y: {
-                display: true,
-                //beginAtZero: false,
-                type: "logarithmic"
-              },
-              x: {
-                display: true
+                type: 'logarithmic'
               }
             }
           }
