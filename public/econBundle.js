3212bc04a4de00e24b1be5c09885de0cd736fd79
Rowan Whelan <96452323+rowandwhelan@users.noreply.github.com>
2024-02-03 13:36:19 -0600

    Update econBundle.js

--- public/econBundle.js
+++ public/econBundle.js
@@ -133,38 +133,8 @@ async function econGet(date) {
   }
 }
   
-var econDataFetched
-var econInit
-var days 
-var iron 
-var exp 
-var flux 
-var rubber 
-var cores 
-var gens 
-var projs 
-var rcs 
-var auto 
-var burst 
-var goldNull 
-var silverNull 
-var bug 
-var shredder 
-var legacy 
-var glass 
-var rcd 
-var backpack 
-var bp 
-var manifest 
-var bom 
-var backpack 
-var volleyball 
-var basketball 
-var beachball 
-var football 
-var rapid 
-var pres 
-var controller 
+let econDataFetched, days, iron, exp, flux, rubber, cores, gens, projs, rcs, auto, burst, goldNull, silverNull, bug, shredder, legacy, glass, rcd, backpack, bp, manifest, bom, backpack, volleyball, basketball, beachball, football, rapid, pres, controller 
+
 function output(econ, items, date) {
   let outputDate = dayjs(date, "YYYY_M_D").format("ddd[,] DD MMM YYYY")
     days.push(outputDate)
@@ -248,25 +218,25 @@ document.addEventListener("change", graphChart);
         graphDataSet.push({ label: 'Iron', data: iron, borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Explosives', data: exp, borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Hyper Rubber', data: rubber , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Flux Crystals', data: flux , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("shields").checked) {
-        graphDataSet.push({ label: 'Shield Cores', data: cores , borderWidth: borderWidth, pointStyle: false }, { label: 'Shield Generators', data: gens , borderWidth: borderWidth, pointStyle: false }, { label: 'Shield Projectors', data: projs , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Shield Cores', data: cores , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Shield Generators', data: gens , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Shield Projectors', data: projs , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("turrets").checked) {
-        graphDataSet.push({ label: 'RC Turret', data: rcs , borderWidth: borderWidth, pointStyle: false }, { label: 'Auto Turret', auto: auto , borderWidth: borderWidth, pointStyle: false }, { label: 'Burst Turret', burst: burst , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'RC Turret', data: rcs , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Auto Turret', auto: auto , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Burst Turret', burst: burst , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("rares").checked) {
-        graphDataSet.push({ label: 'Gold Null Trophy', data: goldNull , borderWidth: borderWidth, pointStyle: false }, { label: 'Silver Null Trophy', data: silverNull , borderWidth: borderWidth, pointStyle: false }, { label: 'Bug Hunter Trophy', data: bug , borderWidth: borderWidth, pointStyle: false }, { label: 'Golden Shredder', data: shredder , borderWidth: borderWidth, pointStyle: false }, { label: 'Legacy Fabricator', data: legacy , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Gold Null Trophy', data: goldNull , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Silver Null Trophy', data: silverNull , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Bug Hunter Trophy', data: bug , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Golden Shredder', data: shredder , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Legacy Fabricator', data: legacy , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("misc").checked) {
-        graphDataSet.push({ label: 'Ice Glass', data: glass , borderWidth: borderWidth, pointStyle: false }, { label: 'Flux RCD', data: rcd , borderWidth: borderWidth, pointStyle: false }, { label: 'Backpack', data: backpack , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Ice Glass', data: glass , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Flux RCD', data: rcd , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Backpack', data: backpack , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("scanners").checked) {
-        graphDataSet.push({ label: 'Blueprint Scanner', data: bp , borderWidth: borderWidth, pointStyle: false }, { label: 'Manifest Scanner', data: manifest , borderWidth: borderWidth, pointStyle: false }, { label: 'BoM Scanner', data: bom , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Blueprint Scanner', data: bp , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Manifest Scanner', data: manifest , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'BoM Scanner', data: bom , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("balls").checked) {
-        graphDataSet.push({ label: 'Volleyball', data: volleyball , borderWidth: borderWidth, pointStyle: false }, { label: 'Basketball', data: basketball , borderWidth: borderWidth, pointStyle: false }, { label: 'Beach Ball', data: beachball , borderWidth: borderWidth, pointStyle: false }, { label: 'Football', data: football , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Volleyball', data: volleyball , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Basketball', data: basketball , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Beach Ball', data: beachball , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Football', data: football , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (document.getElementById("vulture").checked) {
-        graphDataSet.push({ label: 'Turret Booster - Rapid Fire', data: rapid , borderWidth: borderWidth, pointStyle: false }, { label: 'Turret Booster - Preservation', data: pres , borderWidth: borderWidth, pointStyle: false }, { label: 'Enhanced Turret Controller', data: controller , borderWidth: borderWidth, pointStyle: false })
+        graphDataSet.push({ label: 'Turret Booster - Rapid Fire', data: rapid , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Turret Booster - Preservation', data: pres , borderWidth: borderWidth, pointRadius: pointRadius }, { label: 'Enhanced Turret Controller', data: controller , borderWidth: borderWidth, pointRadius: pointRadius })
       }
       if (Chart.getChart("chart") !=  undefined){
       Chart.getChart("chart").destroy();
