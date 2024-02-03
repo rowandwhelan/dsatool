e355a03d5c48353a22e20c1f354c34cd8849e760
Rowan Whelan <96452323+rowandwhelan@users.noreply.github.com>
2024-02-03 14:19:37 -0600

    Update darkmode.js

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,16 +1,18 @@
-// Dark Mode Toggle
+// Dark Mode Toggle (TODO: Fix)
+/*
 const darkModeToggle =  document.getElementById("darkMode")
-(function swapViewingMode() {
+function swapViewingMode() {
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('darkMode', 'enabled');
   } else {
     localStorage.setItem('darkMode', 'disabled');
   }
-});
+};
 
 // Check local storage for dark mode setting
 if (localStorage.getItem('darkMode') === 'enabled') {
   document.body.classList.add('dark-mode');
 }
 document.body.appendChild(darkModeToggle);
+*/
