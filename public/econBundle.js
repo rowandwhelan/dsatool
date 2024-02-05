f785cb8859b2a18787cde88eea766163aa0e0ede
Rowan Whelan <96452323+rowandwhelan@users.noreply.github.com>
2024-02-05 11:29:15 -0600

    Update econBundle.js

--- public/econBundle.js
+++ public/econBundle.js
@@ -256,6 +256,7 @@ document.addEventListener("change", graphChart);
           labels: days,
           datasets: graphDataSet,
           options: {
+            maintainAspectRatio: false,
             interaction: {
               mode: 'x',
               intersection: false
