4c84991ac825d55e0509c74dd19057abedf088e0
Rowan Whelan <96452323+rowandwhelan@users.noreply.github.com>
2024-07-09 22:22:51 -0500

    Update calc.js

--- src/calc.js
+++ src/calc.js
@@ -273,7 +273,7 @@ function calculateCoreConsumption() {
 }
 
 function calculateMatConsumption(){
-  document.getElementById("metalConsumed").innerText = `Metal: ${ParseInt((Math.ceil(document.getElementById("ammoConsumptionRate").value * document.getElementById("numTurrets").value * (document.getElementById("firePercentage").value / 100) * 0.5 * 60 * 20 / 4))) + ParseInt(document.getElementById("matsRequiredForBoosters").value)}`
+  document.getElementById("metalConsumed").innerText = `Metal: ${(Math.ceil(document.getElementById("ammoConsumptionRate").value * document.getElementById("numTurrets").value * (document.getElementById("firePercentage").value / 100) * 0.5 * 60 * 20 / 4)) + ParseInt(document.getElementById("matsRequiredForBoosters").value)}`
   document.getElementById("expConsumed").innerText = `Explosives: ${Math.ceil(document.getElementById("ammoConsumptionRate").value * document.getElementById("numTurrets").value * (document.getElementById("firePercentage").value / 100) * 0.5 * 60 * 20 / 4)}`
   if (document.querySelector('input[name="coresOrBoosters"]:checked').value === "cores"){
   document.getElementById("coresConsumed").innerText = `Cores: ${document.getElementById("coreConsumption").value * 20}`
